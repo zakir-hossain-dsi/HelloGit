02d007faeaa83dff77ccdb1d3b810b27744ecc92
zakir-hossain-dsi <zakir.hossain@dsinv.com>
2014-02-04 22:00:18 +0600

    EGit Test Commit.

---
+++ HelloGit/src/com/dsi/execution/Execute.java
@@ -0,0 +1,12 @@
+package com.dsi.execution;
+
+public class Execute {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Egit..");
+	}
+}
