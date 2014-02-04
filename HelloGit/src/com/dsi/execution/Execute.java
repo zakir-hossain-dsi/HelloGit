fa3e56c3ac895f35c882427a6d2a23f4778d208d
zakir-hossain-dsi <zakir.hossain@dsinv.com>
2014-02-04 22:10:12 +0600

    Add another print line.

--- HelloGit/src/com/dsi/execution/Execute.java
+++ HelloGit/src/com/dsi/execution/Execute.java
@@ -8,5 +8,6 @@ public class Execute {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Egit..");
+		System.out.println("Hello Egit..");
 	}
 }
