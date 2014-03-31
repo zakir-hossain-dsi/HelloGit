a00bda280f74f716f84fff0c0cbed00ebfcc4b08
zakir-hossain-dsi <zakir.hossain@dsinv.com>
2014-03-31 18:22:40 +0600

    Remove multiple println statement.

--- HelloGit/src/com/dsi/execution/Execute.java
+++ HelloGit/src/com/dsi/execution/Execute.java
@@ -7,15 +7,6 @@ public class Execute {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Egit..");
-		System.out.println("Hello Egit..");
 		System.out.println("Another Line");
-		
-		System.out.println("Hello Egit..");
-		System.out.println("Another Line");
-
-		System.out.println("Hello Egit..");
-		System.out.println("Another Line");
-		
 	}
 }
